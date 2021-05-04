ba800ff326f9b5b3420aff9353a15d4cc334102b
Ismael Lastre Alvarez <ismaelfac@gmail.com>
2021-05-04 08:32:32 -0500

    Base Project

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,10 +39,11 @@
               @endif
             @else
               <div class="social-media">
-                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                    {{ Auth::user()->name }}
+                <a id="navbarDropdown" class="btn btn-primary btn-sm mr-2 nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    Bienvenido <br>{{ Auth::user()->name }}
                 </a>
                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                    <a href="#" class="dropdown-item">Mi portal </a>
                     <a class="dropdown-item" href="{{ route('logout') }}"
                         onclick="event.preventDefault();
                                       document.getElementById('logout-form').submit();">
