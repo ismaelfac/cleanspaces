1f873b0748d13d45c3f62b53d1f502fb1fbdb0f6
ISMAEL E. LASTRE ALVAREZ <ismaelfac@gmail.com>
2021-05-19 20:06:48 -0500

    modules services

--- database/migrations/2021_04_031_022614_create_services_table.php
+++ database/migrations/2021_04_031_022614_create_services_table.php
@@ -16,7 +16,7 @@ class CreateServicesTable extends Migration
         Schema::create('services', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('description');
+            $table->mediumText('description');
             $table->string('slug')->unique();
             $table->boolean('state')->default(false);
         });
